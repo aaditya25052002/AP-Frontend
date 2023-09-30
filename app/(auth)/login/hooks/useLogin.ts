5ffbf396a0fed61f963d939a5fff977684fb0ec6
Aditya Tiwari <pubgmlord25@gmail.com>
2023-09-30 23:13:27 +0530

    types changes

--- app/(auth)/login/hooks/useLogin.ts
+++ app/(auth)/login/hooks/useLogin.ts
@@ -1,21 +1,31 @@
 import { useRouter } from 'next/router';  
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+
+// Separate groups of imports with an empty line
 import { useSupabase } from "@/lib/context/SupabaseProvider";
 import { useToast } from "@/lib/hooks";
 import { useEventTracking } from "@/services/analytics/useEventTracking";
 
-export const useLogin = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isPending, setIsPending] = useState(false);
+export const useLogin = (): {
+  handleLogin: () => Promise<void>,
+  setEmail: React.Dispatch<React.SetStateAction<string>>,
+  setPassword: React.Dispatch<React.SetStateAction<string>>,
+  email: string,
+  isPending: boolean,
+  password: string
+} => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [isPending, setIsPending] = useState<boolean>(false);
   const { publish } = useToast();
   const { supabase, session } = useSupabase();
-
   const { track } = useEventTracking();
   const { t } = useTranslation(["login"]);
   const router = useRouter();
-  const slackId = router.query.teamId?.toString() || "";  // Assuming `teamId` might be non-string
+
+  // Use nullish coalescing
+  const slackId = router.query.teamId?.toString() ?? "";
 
   const handleLogin = async () => {
     setIsPending(true);
@@ -24,14 +34,14 @@ export const useLogin = () => {
     if (error) {
       console.log(error.message);
       if (error.message.includes("Failed")) {
-        publish({ variant: "danger", text: t("Failedtofetch",{ ns: 'login' }) });
+        publish({ variant: "danger", text: t("Failedtofetch", { ns: 'login' }) });
       } else if (error.message.includes("Invalid")) {
-        publish({ variant: "danger", text: t("Invalidlogincredentials",{ ns: 'login' }) });
+        publish({ variant: "danger", text: t("Invalidlogincredentials", { ns: 'login' }) });
       } else {
         publish({ variant: "danger", text: error.message });
       }
-    } else if (data && data.session) {
-      publish({ variant: "success", text: t("loginSuccess",{ ns: 'login' }) });
+    } else if (data?.session) {  // Use optional chaining
+      publish({ variant: "success", text: t("loginSuccess", { ns: 'login' }) });
       const accessToken = data.session.access_token;
       if (slackId.trim() && accessToken) {
         const insertResult = await supabase
@@ -48,18 +58,18 @@ export const useLogin = () => {
   };
 
   useEffect(() => {
-    if (session && session.user) {
+    if (session?.user) {  // Use optional chaining
       track("SIGNED_IN").catch(console.error);
       
       const previousPage = sessionStorage.getItem("previous-page");
-      if (!previousPage || previousPage.trim() === "") {
+      if (!previousPage || previousPage.trim() === "") {  // Explicit empty string check
         void router.push("/upload");
       } else {
         sessionStorage.removeItem("previous-page");
         void router.push(previousPage);
       }
     }
-  }, [session, session?.user, router, track]);
+  }, [session?.user, router, track]);  // Use optional chaining
 
   return {
     handleLogin,
